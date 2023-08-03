cf7492ee77f289406785ea55a98afc247ff7cc0c
DiazGab <108848133+DiazGab@users.noreply.github.com>
2023-08-03 00:36:18 -0300

    Express y un poco más

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const path = require('path');
+
+const app = express();
+
+app.listen(3000, () => console.log('Sirve'));
+
+app.get('/', function(req, res) {
+    let htmlPath = path.resolve(__dirname, './views/index.html');
+    res.sendFile(htmlPath);
+})
+
