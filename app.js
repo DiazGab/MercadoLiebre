33e02efa2742395b83757e52765a7320f166e055
DiazGab <108848133+DiazGab@users.noreply.github.com>
2023-08-05 00:46:03 -0300

    Progresando sobre estilos

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 app.listen(3000, () => console.log('Sirve'));
 
 app.get('/', function(req, res) {
-    let htmlPath = path.resolve(__dirname, './views/index.html');
+    let htmlPath = path.resolve(__dirname, './views/home.html');
     res.sendFile(htmlPath);
 })
 
