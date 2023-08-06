c4d3160185d36393f5406fac3c3a07fa46937da0
DiazGab <108848133+DiazGab@users.noreply.github.com>
2023-08-06 19:49:14 -0300

    Agregando urls a la app

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const express = require('express');
 
+const app = express();
+
 const path = require('path');
 
-const app = express();
+app.use(express.static('public'));
 
 app.listen(3000, () => console.log('Sirve'));
 
 app.get('/', function(req, res) {
     let htmlPath = path.resolve(__dirname, './views/home.html');
     res.sendFile(htmlPath);
+})
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/register.html'))
 })
 
